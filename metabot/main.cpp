3e73d9e805b0182a576a84d1db07a73fb854dfc6
Gregwar <g.passault@gmail.com>
2014-10-21 16:32:24 +0200

    -s to export the robot as stl

--- metabot/main.cpp
+++ metabot/main.cpp
@@ -12,12 +12,35 @@
 using namespace std;
 using namespace Metabot;
 
+Backend *backend = NULL;
+Robot *robot = NULL;
+string robotFile = "";
+
 void usage()
 {
     cerr << "Metabot v1.0 - Rhoban System" << endl;
+    cerr << "Usage: metabot [option] [file.robot]" << endl;
     cerr << "" << endl;
     cerr << "   -c: Clears the cache" << endl;
     cerr << "   -w: Warmup/generates the cache" << endl;
+    cerr << "   -s [output.stl]: Export the given robot to stl" << endl;
+    exit(EXIT_FAILURE);
+}
+
+void needRobot()
+{
+    if (robotFile != "") {
+        try {
+            robot = new Robot(backend);
+            robot->loadFromFile(robotFile);
+        } catch (...) {
+            cerr << "Error: unable to open " << robotFile << endl;
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        cerr << "Error: no robot given" << endl;
+        exit(EXIT_FAILURE);
+    }
 }
 
 int main(int argc, char *argv[])
@@ -25,8 +48,10 @@ int main(int argc, char *argv[])
     int index;
     bool cacheClear = false;
     bool cacheWarmup = false;
+    bool stlExport = false;
+    string output;
 
-    while ((index = getopt(argc, argv, "cw")) != -1) {
+    while ((index = getopt(argc, argv, "cws:")) != -1) {
         switch (index) {
             case 'c':
                 cacheClear = true;
@@ -34,23 +59,37 @@ int main(int argc, char *argv[])
             case 'w':
                 cacheWarmup = true;
                 break;
+            case 's':
+                stlExport = true;
+                output = string(optarg);
+                break;
         }
+    }
+
+    if (optind != argc) {
+        robotFile = string(argv[optind]);
     }
 
     try {
         // Loading the backend
-        Backend backend("xl-320");
-        backend.load();
+        backend = new Backend("xl-320");
+        backend->load();
 
-        // Cache warmup
+        // Cache handling
         if (cacheClear) {
             cout << "Clearing the cache" << endl;
-            int n = backend.clearCache();
+            int n = backend->clearCache();
             cout << "Done, removed " << n << " files." << endl;
         } else if (cacheWarmup) {
             cout << "Generating the cache..." << endl;
-            backend.buildCache();
-            cout << "Cache generated (" << backend.cacheFiles() << " files)." << endl;
+            backend->buildCache();
+            cout << "Cache generated (" << backend->cacheFiles() << " files)." << endl;
+        // Robot handling
+        } else if (stlExport) {
+            needRobot();
+            cout << "Saving robot to " << output << endl;
+            auto model = robot->toModel();
+            saveModelToFileBinary(output.c_str(), &model);
         } else {
             usage();
         }
@@ -58,6 +97,14 @@ int main(int argc, char *argv[])
     } catch (string error) {
         cerr << "[ERROR] " << error << endl;
         return EXIT_FAILURE;
+    }
+
+    if (robot != NULL) {
+        delete robot;
+    }
+
+    if (backend != NULL) {
+        delete backend;
     }
 
     return EXIT_SUCCESS;
