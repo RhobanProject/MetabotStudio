2bd2fc908062d2edca7aa7b35b26d2523978f692
Gregwar <g.passault@gmail.com>
2015-01-05 09:47:34 +0100

    Quality to 20

--- metabot/Module.cpp
+++ metabot/Module.cpp
@@ -124,7 +124,7 @@ namespace Metabot
         if (noModels) {
             cmd << "-DNoModels=true ";
         }
-        cmd << "-D\\$fn=45 ";
+        cmd << "-D\\$fn=20 ";
         cmd << input << " -o " << output;
         // cmd << " >/dev/null 2>/dev/null";
         std::string command = cmd.str();
