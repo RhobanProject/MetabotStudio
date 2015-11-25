63fd400567a81354953453aca9204d55b520f205
Gregwar <g.passault@gmail.com>
2015-11-25 15:34:32 +0100

    benchmark

---
+++ metabot/util/Benchmark.cpp
@@ -0,0 +1,144 @@
+#include "Benchmark.hpp"
+
+#include <algorithm>
+#include <exception>
+#include <iomanip>
+#include <iostream>
+#include <tuple>
+
+using namespace std::chrono;
+
+/* Static variables */
+Benchmark * Benchmark::current = NULL;
+
+    Benchmark::Benchmark(Benchmark * f, const std::string & n)
+: father(f), name(n)
+{
+#ifndef WIN32
+    openingTime = steady_clock::now();
+#endif
+    isTimerActive = false;
+}
+
+Benchmark * Benchmark::getCurrent()
+{
+    if (current == NULL)
+        open("Default");
+    return current;
+}
+
+Benchmark::~Benchmark()
+{
+    for (auto& c : children) {
+        delete(c.second);
+    }
+}
+
+void Benchmark::open(const std::string& benchmarkName)
+{
+    Benchmark * newB = new Benchmark(current, benchmarkName);
+    if (current != NULL) {
+        current->children.push_back(namedBenchmark(benchmarkName, newB));
+    }
+    current = newB;
+}
+
+double Benchmark::close(bool print, int detailLevel, std::ostream & out)
+{
+    if (current == NULL)
+        throw std::runtime_error("No active benchmark to close");
+    Benchmark * toClose = current;
+#ifndef WIN32
+    toClose->closingTime = steady_clock::now();
+#endif
+    current = toClose->father;
+    if (print)
+        toClose->print(out, detailLevel);
+
+    double elapsedTime = toClose->getTime();
+    // Suppress Benchmark if the link is lost
+    if (current == NULL)
+        delete(toClose);
+    return elapsedTime;
+}
+
+double Benchmark::getTime() const
+{
+    double elapsedTicks = double((closingTime - openingTime).count());
+    double time = elapsedTicks * steady_clock::period::num / steady_clock::period::den;
+    return time;
+
+}
+
+double Benchmark::getSubTime() const
+{
+    double t = 0;
+    for (auto& c : children) {
+        t += c.second->getTime();
+    }
+    return t;
+}
+
+
+// Printable data: <name, elapsedTime, link>
+typedef std::tuple<std::string, double, Benchmark *> printableEntry;
+int cmp(const printableEntry& a,
+        const printableEntry& b)
+{
+    return std::get<1>(a) < std::get<1>(b);
+}
+
+void Benchmark::print(std::ostream & out, int maxDepth)
+{
+    // Formatting specifically
+    int precision = 3;
+    int width = 8;
+    int oldPrecision = out.precision();
+    out.precision(precision);
+    out.setf(std::ios::fixed, std::ios::floatfield);
+
+    print(out, 0, width, maxDepth);
+
+    // Restoring default
+    out.precision(oldPrecision);
+    out.unsetf(std::ios::floatfield);
+}
+
+void Benchmark::print(std::ostream & out, int depth, int width,
+        int maxDepth)
+{
+    // Build and sort printable data
+    typedef std::tuple<std::string, double, Benchmark *> printableEntry;
+    std::vector<printableEntry> subFields;
+    for(auto& c : children) {
+        subFields.push_back(printableEntry(c.first,
+                    c.second->getTime(),
+                    c.second));
+    }
+    // Add Unknown field only if there are other information
+    if (subFields.size() > 0) {
+        subFields.push_back(printableEntry("Unknown",
+                    getTime() - getSubTime(),
+                    NULL));
+    }
+    // Ordering
+    std::sort(subFields.begin(), subFields.end(), cmp);
+    // Printing if allowed
+    if (maxDepth < 0 || depth < maxDepth) {
+        for(auto& f : subFields) {
+            // For Benchmark, print them
+            if (std::get<2>(f) != NULL){
+                std::get<2>(f)->print(out, depth + 1, width, maxDepth);
+            }
+            // Print entries
+            else {
+                for (int i = 0; i < depth + 1; i++) out << '\t';
+                out << std::setw(width) << (std::get<1>(f) * 1000) << " ms : "
+                    << std::get<0>(f) << std::endl;
+            }
+        }
+    }
+    for (int i = 0; i < depth; i++) out << '\t';
+    out << std::setw(width) << getTime() * 1000 << " ms : "
+        << name << std::endl;
+}
---
+++ metabot/util/Benchmark.hpp
@@ -0,0 +1,56 @@
+#ifndef UTILS_TIMING_BENCHMARK_HPP
+#define UTILS_TIMING_BENCHMARK_HPP
+
+#include <chrono>
+#include <map>
+#include <vector>
+#include <string>
+#include <stdexcept>
+#include <iostream>
+// TODO: create an example tests for this class
+
+class Benchmark
+{
+    private:
+        /* Static variables */
+        static Benchmark * current;
+
+        // typedefs
+        typedef std::chrono::time_point<std::chrono::steady_clock> TimeStamp;
+        typedef std::pair<std::string, Benchmark *> namedBenchmark;
+        /* Local variables */
+        Benchmark * father;
+        std::string name;
+        TimeStamp openingTime;
+        TimeStamp closingTime;
+        bool isTimerActive;
+        std::vector<namedBenchmark> children;
+
+        static Benchmark * getCurrent();
+
+        void print(std::ostream & out, int maxDepth = -1);
+        void print(std::ostream & out, int depth, int width , int maxDepth = -1);
+
+        double getTime() const;
+        double getSubTime() const;
+
+    public:
+        Benchmark(Benchmark * father, const std::string & name);
+        ~Benchmark();
+        //TODO fix memory leak due to destruction of children
+
+        /**
+         * Open a new benchmark or subBenchmark
+         * It is not recommended to open several subBenchmark with the same name
+         */
+        static void open(const std::string& benchmarkName);
+
+        /**
+         * Close current benchmark or subBenchmark and return to previous context
+         * if needed
+         */
+        static double close(bool print = false, int detailLevel = -1,
+                std::ostream & out = std::cout);
+};
+
+#endif//UTILS_TIMING_BENCHMARK_HPP
