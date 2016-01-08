417649ae835045340d6f738df287b44c49b032bb
Gregwar <g.passault@gmail.com>
2016-01-08 11:39:16 +0100

    Fixing fevals

--- simulation/main.cpp
+++ simulation/main.cpp
@@ -109,6 +109,7 @@ int main(int argc, char *argv[])
             cmaparams.set_algo(BIPOP_CMAES);
             cmaparams.set_quiet(false);
             cmaparams.set_max_iter(100);
+            cmaparams.set_max_fevals(10000000);
             cmaparams.set_elitism(2);
             cmaparams.set_mt_feval(true);
             cmaparams.set_ftarget(0.0);
