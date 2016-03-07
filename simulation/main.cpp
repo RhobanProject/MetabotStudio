a5af1baa35f957c75b8127177282bec6b23579f5
Gregwar <g.passault@gmail.com>
2016-03-07 09:28:27 +0100

    sim

--- simulation/main.cpp
+++ simulation/main.cpp
@@ -187,7 +187,8 @@ int main(int argc, char *argv[])
             cmaparams.set_restarts(3);
             //cmaparams.set_max_hist(3);
             //cmaparams.set_max_fevals(10000000);
-            cmaparams.set_elitism(1);
+            cmaparams.set_elitism(3);
+            cmaparams.set_x0(0, 1);
             //cmaparams.set_ftolerance(0.00001);
             cmaparams.set_mt_feval(true);
             cmaparams.set_ftarget(0.0);
