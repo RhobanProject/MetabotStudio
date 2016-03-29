d952f2ba97528001d84ff1d716a5e289e2e4daf7
Gregwar <g.passault@gmail.com>
2016-03-29 22:54:49 +0200

    0 0

--- simulation/ExperimentStandUp.cpp
+++ simulation/ExperimentStandUp.cpp
@@ -15,7 +15,7 @@ std::vector<std::string> ExperimentStandUp::splineNames()
 void ExperimentStandUp::initParameters(Parameters &parameters, Metabot::Robot *robot)
 {
     for (auto name : splineNames()) {
-        for (int t=0; t<=5; t++) {
+        for (int t=1; t<=5; t++) {
             std::stringstream ss;
             ss << name << "_" << t;
             parameters.add(ss.str(), -150, 150, 0);
@@ -47,7 +47,8 @@ void ExperimentStandUp::init(Parameters &parameters, Metabot::Robot *robot)
     // Loading splines
     for (auto name : splineNames()) {
         Function f;
-        for (int t=0; t<=6; t++) {
+        f.addPoint(0, 0);
+        for (int t=1; t<=6; t++) {
             std::stringstream ss;
             ss << name << "_" << t;
             f.addPoint(t, parameters.get(ss.str()));
